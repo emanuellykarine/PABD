02917c65883b50e746878f8748a17cb0dc6bd64a
Emanuelly Karine Fernandes dos Santos <20241014040009@ifrn.local>
2025-10-29 10:23:54 -0300

    inicio projeto tarefa

--- projeto_tarefa/projeto/views.py
+++ projeto_tarefa/projeto/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from .models import Projeto, Tarefa, Usuario
 from .serializers import ProjetoSerializer, TarefaSerializer, UsuarioSerializer
+from rest_framework.decorators import authentication_classes, permission_classes, api_view # type: ignore
+from rest_framework.permissions import IsAuthenticated  # type: ignore
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication # type: ignore
+from django.shortcuts import get_object_or_404 # type: ignore
 
 # Create your views here.
 
@@ -22,3 +28,31 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     serializer_class = UsuarioSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['username', 'email']
+
+    @api_view(['POST'])
+    def login(request):
+        user = get_object_or_404(Usuario, username=request.data['username'])
+        if not user.check_password(request.data['password']):
+            return Response({'message': 'Not Found'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        token, created = Token.objects.get_or_create(user=user)
+        serializer = UsuarioSerializer(instance=user)
+        return Response({'token': token.key, 'user':serializer.data})
+
+    @api_view(['POST'])
+    def signup(request):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            user = Usuario.objects.get(username=request.data['username'])
+            user.set_password(request.data['password'])
+            user.save()
+            token = Token.object.create(user=user)
+            return Response({'token': token.key, 'user':serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @api_view(['GET'])
+    @authentication_classes([TokenAuthentication, SessionAuthentication])
+    @permission_classes([IsAuthenticated])
+    def test_token(request):
+        return Response("passou para {}".format(request.user.email))
